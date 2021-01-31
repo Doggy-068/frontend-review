a4510aa4fd1ece49a67235a5bd1510147fa8d02c
Doggy <728724654@qq.com>
2021-01-31 21:18:12 +0800

    添加 发布-订阅模式

--- design/README.md
+++ design/README.md
@@ -4,3 +4,4 @@
 * [策略模式](strategy.md)
 * [代理模式](proxy.md)
 * [迭代器模式](iteration.md)
+* [发布-订阅模式](publish-subscribe.md)
---
+++ example/design/publish-subscribe.js
@@ -0,0 +1,34 @@
+class PubSubEvent {
+  constructor() {
+    this.clientList = {}
+  }
+  listen(key, fn) {
+    if (!this.clientList[key]) {
+      this.clientList[key] = []
+    }
+    this.clientList[key].push(fn)
+  }
+  trigger(...args) {
+    const key = args.shift()
+    const fns = this.clientList[key]
+    if (!fns || fns.length === 0) {
+      return false
+    }
+    fns.forEach(fn => fn(...args))
+  }
+  remove(key, fn) {
+    const fns = this.clientList[key]
+    if (!fns) {
+      return false
+    }
+    if (!fn) {
+      fns.length = 0
+    } else {
+      for (let i = 0, len = fns.length; i < len; i++) {
+        if (fn === fns[i]) {
+          fns.splice(i, 1)
+        }
+      }
+    }
+  }
+}
